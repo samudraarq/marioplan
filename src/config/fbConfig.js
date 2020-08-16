3f882e719a378c8973ebf69a23f1a3264d71f2cc
samudrafaris <samudrafaris@gmail.com>
2020-08-16 18:57:02 +0700

    setup firebase

---
+++ src/config/fbConfig.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+  apiKey: "AIzaSyA9olSz2VoVpR8xOMQMYxSJqvSdYl9gjv8",
+  authDomain: "thenetninja-marioplan-e715e.firebaseapp.com",
+  databaseURL: "https://thenetninja-marioplan-e715e.firebaseio.com",
+  projectId: "thenetninja-marioplan-e715e",
+  storageBucket: "thenetninja-marioplan-e715e.appspot.com",
+  messagingSenderId: "1094638653066",
+  appId: "1:1094638653066:web:50881ed9c68f1724c3a773",
+  measurementId: "G-P2B9WY7ZWH",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settings({ timestampsInSnapshots: true });
+
+export default firebase;
